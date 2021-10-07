9c49ada20d4c2f441f5d29da2c43b090bb1b4ee8
Adrian Hong <Adrian.Hong@team.telstra.com>
2021-10-08 00:17:37 +1100

    :sparkles: Added radius select

--- frontend/src/components/radiusSelect/RadiusSelect.jsx
+++ frontend/src/components/radiusSelect/RadiusSelect.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import { Select } from "antd";
 import "./RadiusSelect.less";
 
 export default function RadiusSelect() {
   const { Option } = Select;
+  const radiusList = ["5", "10", "15", "20", "25"];
+  const [radius, setRadius] = useState(radiusList[0]);
 
   return (
     <>
       <div className="radius">
-        <Select defaultValue="5" style={{ width: 120 }}>
-          <Option value="5">5</Option>
-          <Option value="10">10</Option>
-          <Option value="15">15</Option>
-          <Option value="20">20</Option>
-          <Option value="25">25</Option>
+        <Select
+          style={{ width: 75 }}
+          onChange={(value) => {
+            setRadius(value);
+          }}
+          value={radius}
+        >
+          <Option value={radiusList[0]} key={radiusList[0]}>
+            {radiusList[0]}
+          </Option>
+          <Option value={radiusList[1]} key={radiusList[1]}>
+            {radiusList[1]}
+          </Option>
+          <Option value={radiusList[2]} key={radiusList[2]}>
+            {radiusList[2]}
+          </Option>
+          <Option value={radiusList[3]} key={radiusList[3]}>
+            {radiusList[3]}
+          </Option>
+          <Option value={radiusList[4]} key={radiusList[4]}>
+            {radiusList[4]}
+          </Option>
         </Select>
       </div>
     </>
